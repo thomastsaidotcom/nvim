a72eead3cb930807aa943790c6cc63b66bab9ca5
Thomas Tsai <me@thomastsai.com>
2025-04-01 18:57:21 -0700

    Color Schemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,26 @@
 return {
-  "catppuccin/nvim",
-  lazy = true,
+  {
+    "sonph/onehalf",
+    config = function(plugin)
+      vim.opt.rtp:append(plugin.dir .. "/vim")
+    end,
+  },
+  {
+    "NLKNguyen/papercolor-theme",
+  },
+  {
+    "nordtheme/vim",
+  },
+  {
+    "ayu-theme/ayu-vim",
+  },
+  {
+    "navarasu/onedark.nvim",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nord",
+    },
+  },
 }
