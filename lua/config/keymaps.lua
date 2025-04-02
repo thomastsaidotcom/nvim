49c569d72e1390ef184d6ec75b023a09aab2d84b
Thomas Tsai <me@thomastsai.com>
2025-04-01 18:59:31 -0700

    Add git log highlighted lines capability

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+function GLogVisual()
+  local start_line = vim.fn.line("'<")
+  local end_line = vim.fn.line("'>")
+  local num_lines = end_line - start_line + 1
+  local cmd = string.format(":G log -L%d,+%d:%%", start_line, num_lines)
+  vim.cmd(cmd)
+end
+
+vim.api.nvim_set_keymap("v", "<leader>gl", ":<C-U>lua GLogVisual()<CR>", { noremap = true, silent = true })
