472948efbbcbdf8550abf97686a06e0d28c2e0a0
Thomas Tsai <me@thomastsai.com>
2025-07-21 21:52:54 -0700

    Add 7 new colorscheme options
    - nightfox.nvim (nightfox, duskfox, nordfox variants)
    - vscode.nvim (VS Code theme)
    - forest-night.nvim
    - ayu-vim-darker
    - papercolor-theme
    - nord.nvim
    - bluloco.nvim (light/dark variants)

---
+++ lua/custom/plugins/colorschemes.lua
@@ -0,0 +1,76 @@
+-- Colorschemes collection
+return {
+  -- Nightfox colorscheme family
+  {
+    "EdenEast/nightfox.nvim",
+    priority = 1000,
+    config = function()
+      require("nightfox").setup({
+        options = {
+          transparent = false,
+          terminal_colors = true,
+        }
+      })
+    end,
+  },
+
+  -- VSCode theme
+  {
+    "Mofiqul/vscode.nvim",
+    priority = 1000,
+    config = function()
+      require("vscode").setup({
+        transparent = false,
+        italic_comments = true,
+        disable_nvimtree_bg = true,
+      })
+    end,
+  },
+
+  -- Forest Night theme
+  {
+    "adibhanna/forest-night.nvim",
+    priority = 1000,
+  },
+
+  -- Ayu Darker theme
+  {
+    "k4yt3x/ayu-vim-darker",
+    priority = 1000,
+  },
+
+  -- PaperColor theme
+  {
+    "nlknguyen/papercolor-theme",
+    priority = 1000,
+  },
+
+  -- Nord theme
+  {
+    "shaunsingh/nord.nvim",
+    priority = 1000,
+    config = function()
+      vim.g.nord_contrast = true
+      vim.g.nord_borders = false
+      vim.g.nord_disable_background = false
+      vim.g.nord_italic = false
+      vim.g.nord_uniform_diff_background = true
+      vim.g.nord_bold = false
+    end,
+  },
+
+  -- Bluloco theme
+  {
+    "uloco/bluloco.nvim",
+    priority = 1000,
+    dependencies = { "rktjmp/lush.nvim" },
+    config = function()
+      require("bluloco").setup({
+        style = "auto",
+        transparent = false,
+        italics = false,
+        terminal = vim.fn.has("gui_running") == 1,
+      })
+    end,
+  },
+}
