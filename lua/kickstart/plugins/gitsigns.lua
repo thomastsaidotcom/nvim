fc120646867b4e8c1dbf50a8b0e1fefc5498103c
Thomas Tsai <me@thomastsai.com>
2025-07-27 16:06:22 -0700

    Fix gitsigns keymap registration by reverting to config function
    Previously broke when using 'opts' format - keymap registration failed.
    Added warning comment to prevent future issues.

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -42,51 +42,54 @@ return {
         desc = 'Previous git hunk'
       },
     },
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-      numhl = true, -- Enable line number highlighting
-      linehl = false, -- Disable line highlighting (we only want line numbers)
-      on_attach = function(bufnr)
-        local gitsigns = require 'gitsigns'
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        -- Actions
-        -- visual mode
-        map('v', '<leader>hs', function()
-          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'git [s]tage hunk' })
-        map('v', '<leader>hr', function()
-          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'git [r]eset hunk' })
-        -- normal mode
-        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
-        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
-        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
-        map('n', '<leader>hu', gitsigns.stage_hunk, { desc = 'git [u]ndo stage hunk' })
-        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
-        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
-        map('n', '<leader>hb', gitsigns.blame_line, { desc = 'git [b]lame line' })
-        map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
-        map('n', '<leader>hD', function()
-          gitsigns.diffthis '@'
-        end, { desc = 'git [D]iff against last commit' })
-        -- Toggles
-        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
-        map('n', '<leader>tD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
-      end,
-    },
     config = function()
+      -- NOTE: Previously broke when moved to 'opts' - keymap registration failed
+      -- Exercise extra care if converting to opts format in the future
+      require('gitsigns').setup({
+        signs = {
+          add = { text = '+' },
+          change = { text = '~' },
+          delete = { text = '_' },
+          topdelete = { text = '‾' },
+          changedelete = { text = '~' },
+        },
+        numhl = true, -- Enable line number highlighting
+        linehl = false, -- Disable line highlighting (we only want line numbers)
+        on_attach = function(bufnr)
+          local gitsigns = require 'gitsigns'
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Actions
+          -- visual mode
+          map('v', '<leader>hs', function()
+            gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+          end, { desc = 'git [s]tage hunk' })
+          map('v', '<leader>hr', function()
+            gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+          end, { desc = 'git [r]eset hunk' })
+          -- normal mode
+          map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
+          map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
+          map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
+          map('n', '<leader>hu', gitsigns.stage_hunk, { desc = 'git [u]ndo stage hunk' })
+          map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
+          map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
+          map('n', '<leader>hb', gitsigns.blame_line, { desc = 'git [b]lame line' })
+          map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
+          map('n', '<leader>hD', function()
+            gitsigns.diffthis '@'
+          end, { desc = 'git [D]iff against last commit' })
+          -- Toggles
+          map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
+          map('n', '<leader>tD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
+        end,
+      })
+
       -- Set up custom highlight groups for line number backgrounds
       vim.api.nvim_set_hl(0, 'GitSignsAddNr', { bg = '#2d5a16', fg = '#ffffff' })
       vim.api.nvim_set_hl(0, 'GitSignsChangeNr', { bg = '#5c3a00', fg = '#ffffff' })
