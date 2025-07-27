17e2e2aea813d3d02a0ca98ff1b5db38b5ebab34
Thomas Tsai <me@thomastsai.com>
2025-07-27 15:29:40 -0700

    Fix mini-statusline active function configuration
    - Fix section function calls to pass proper args objects with trunc_width
    - Restructure active function to avoid array nesting issues
    - Update custom section_mode function to accept args parameter
    - Reorder statusline sections for better layout

--- lua/kickstart/plugins/mini-config/mini-statusline.lua
+++ lua/kickstart/plugins/mini-config/mini-statusline.lua
@@ -3,34 +3,44 @@
 --  and try some other statusline plugin
 local statusline = require("mini.statusline")
 -- set use_icons to true if you have a Nerd Font
-statusline.setup({ use_icons = vim.g.have_nerd_font })
+statusline.setup({
+	use_icons = vim.g.have_nerd_font,
+	content = {
+		active = function()
+			local mode = statusline.section_mode({ trunc_width = 120 })
+			local fileinfo = statusline.section_fileinfo({ trunc_width = 120 })
+			local git = statusline.section_git({ trunc_width = 40 })
+			local diagnostics = statusline.section_diagnostics({ trunc_width = 75 })
 
--- You can configure sections in the statusline by overriding their
--- default behavior. For example, here we set the section for
--- cursor location to LINE:COLUMN
----@diagnostic disable-next-line: duplicate-set-field
-statusline.section_location = function()
-	return "%2l:%-2v"
-end
+			return statusline.combine_groups({
+				mode,
+				"%=",
+				fileinfo,
+				git,
+				diagnostics,
+			})
+		end,
+	},
+})
 
 -- Shorten mode indicator to 2 letters with comments
 ---@diagnostic disable-next-line: duplicate-set-field
-statusline.section_mode = function()
+statusline.section_mode = function(args)
 	local mode_map = {
-		n = "No",     -- Normal mode
-		i = "In",     -- Insert mode
-		v = "Vi",     -- Visual mode
-		V = "VL",     -- Visual Line mode
+		n = "No", -- Normal mode
+		i = "In", -- Insert mode
+		v = "Vi", -- Visual mode
+		V = "VL", -- Visual Line mode
 		["\22"] = "VB", -- Visual Block mode (Ctrl-V)
-		c = "Co",     -- Command mode
-		s = "Se",     -- Select mode
-		S = "SL",     -- Select Line mode
+		c = "Co", -- Command mode
+		s = "Se", -- Select mode
+		S = "SL", -- Select Line mode
 		["\19"] = "SB", -- Select Block mode (Ctrl-S)
-		R = "Re",     -- Replace mode
-		r = "Pr",     -- Prompt mode
+		R = "Re", -- Replace mode
+		r = "Pr", -- Prompt mode
 		["!"] = "Sh", -- Shell mode
-		t = "Te"      -- Terminal mode
+		t = "Te", -- Terminal mode
 	}
 	local mode = vim.api.nvim_get_mode().mode
-	return mode_map[mode] or mode:upper():sub(1,2)
+	return mode_map[mode] or mode:upper():sub(1, 2)
 end
