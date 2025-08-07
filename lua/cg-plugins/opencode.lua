1f068f6c6c9e3f5cc23a1e646ef52cb424589444
Thomas Tsai <me@thomastsai.com>
2025-08-07 09:37:36 -0700

    Add OpenCode.nvim plugin to cg-plugins
    Installs NickvanDyke/opencode.nvim with <leader>a keybinding for opening files in external editor.

---
+++ lua/cg-plugins/opencode.lua
@@ -0,0 +1,8 @@
+-- OpenCode integration for Neovim
+-- OpenCode functionality from Neovim
+return {
+	"NickvanDyke/opencode.nvim",
+	keys = {
+		{ "<leader>a", "<cmd>OpenCode<cr>", desc = "OpenCode" },
+	},
+}
