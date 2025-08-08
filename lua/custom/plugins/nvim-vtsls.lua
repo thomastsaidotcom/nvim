bde7874da7cff7d96cbb4ba394bbe5fe25bc35a6
Thomas Tsai <me@thomastsai.com>
2025-08-08 09:10:57 -0700

    Update nvim-vtsls configuration formatting and add additional options
    Applied consistent formatting with double quotes and proper indentation. Added refactor_auto_rename and refactor_move_to_file configuration options.

--- lua/custom/plugins/nvim-vtsls.lua
+++ lua/custom/plugins/nvim-vtsls.lua
@@ -1,44 +1,52 @@
 return {
-  'yioneko/nvim-vtsls',
-  ft = {
-    'javascript',
-    'javascriptreact', 
-    'javascript.jsx',
-    'typescript',
-    'typescriptreact',
-    'typescript.tsx'
-  },
-  dependencies = {
-    'neovim/nvim-lspconfig'
-  },
-  config = function()
-    require('vtsls').config({
-      settings = {
-        complete_function_calls = true,
-        vtsls = {
-          enableMoveToFileCodeAction = true,
-          autoUseWorkspaceTsdk = true,
-          experimental = {
-            completion = {
-              enableServerSideFuzzyMatch = true,
-            },
-          },
-        },
-        typescript = {
-          updateImportsOnFileMove = { enabled = 'always' },
-          suggest = {
-            completeFunctionCalls = true,
-          },
-          inlayHints = {
-            enumMemberValues = { enabled = true },
-            functionLikeReturnTypes = { enabled = true },
-            parameterNames = { enabled = 'literals' },
-            parameterTypes = { enabled = true },
-            propertyDeclarationTypes = { enabled = true },
-            variableTypes = { enabled = false },
-          },
-        },
-      },
-    })
-  end,
+	"yioneko/nvim-vtsls",
+	ft = {
+		"javascript",
+		"javascriptreact",
+		"javascript.jsx",
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
+	},
+	dependencies = {
+		"neovim/nvim-lspconfig",
+	},
+	config = function()
+		require("vtsls").config({
+			-- automatically trigger renaming of extracted symbol
+			refactor_auto_rename = true,
+			refactor_move_to_file = {
+				-- If dressing.nvim is installed, telescope will be used for selection prompt. Use this to customize
+				-- the opts for telescope picker.
+				telescope_opts = function(items, default) end,
+			},
+			settings = {
+				complete_function_calls = true,
+				vtsls = {
+					enableMoveToFileCodeAction = true,
+					autoUseWorkspaceTsdk = true,
+					experimental = {
+						completion = {
+							enableServerSideFuzzyMatch = true,
+						},
+					},
+				},
+				typescript = {
+					updateImportsOnFileMove = { enabled = "always" },
+					suggest = {
+						completeFunctionCalls = true,
+					},
+					inlayHints = {
+						enumMemberValues = { enabled = true },
+						functionLikeReturnTypes = { enabled = true },
+						parameterNames = { enabled = "literals" },
+						parameterTypes = { enabled = true },
+						propertyDeclarationTypes = { enabled = true },
+						variableTypes = { enabled = false },
+					},
+				},
+			},
+		})
+	end,
 }
+
